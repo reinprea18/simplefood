42bdf657a756a248aa46093bf1931a497b0578a0
schernge18 <raphael.scherngell@edu.fh-joanneum.at>
2020-12-02 10:20:30 +0100

    Some Commit

--- Backend/simplefood/urls.py
+++ Backend/simplefood/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
