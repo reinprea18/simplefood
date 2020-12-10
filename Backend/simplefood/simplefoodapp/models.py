f2af59cb937d8b8bc4416174b41c1b1ad41c6b6b
mfreidl54 <michael.freidl3@edu.fh-joanneum.at>
2020-12-10 10:49:06 +0100

    User

--- Backend/simplefood/simplefoodapp/models.py
+++ Backend/simplefood/simplefoodapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
 
 
 class Restaurant(models.Model):
@@ -25,36 +25,8 @@ class Restaurant(models.Model):
         return "%s%s" % (self.name, self.category)
 
 
-class MyCustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
-        if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
 
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password,
-            username=username,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-
-
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
 
     CHOICES_GENDER = (
         ('m', 'male'),
@@ -69,19 +41,8 @@ class CustomUser(AbstractBaseUser):
         ('t', 'table')
     )
 
-    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
-    username = models.TextField(max_length=30, unique=True)
-    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    password = models.TextField()
-    first_name = models.TextField(null = True)
-    last_name = models.TextField(null = True)
-    date_of_birth = models.DateField(null = True, blank=True, default=False)
-    gender = models.CharField(max_length=1,choices=CHOICES_GENDER, null = True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1,choices=CHOICES_GENDER, null=True)
     street_address = models.TextField(null = True)
     postcode = models.CharField(max_length=4, null = True)
     town = models.TextField(null = True)
@@ -90,19 +51,7 @@ class CustomUser(AbstractBaseUser):
     role = models.CharField(max_length=1, choices=CHOICES_ROLES)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, null=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', ]
 
-    objects = MyCustomUserManager()
-
-    def __str__(self):
-        return "%s%s" % (self.first_name, self.last_name)
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return True
 
 
 class MenuItem(models.Model):
