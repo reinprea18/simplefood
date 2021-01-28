efa6bb59dc88b172a184dbc97a53edad16e99f0b
mfreidl54 <michael.freidl3@edu.fh-joanneum.at>
2021-01-28 17:28:25 +0100

    Changes

--- Frontend/src/app/Components/order-cart/cart-list/cart-list.component.ts
+++ Frontend/src/app/Components/order-cart/cart-list/cart-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {CartItem} from "../../../models/cart-item";
-import {MsngrService} from "../../../services/msngr.service";
-import {Product} from "../../../models/product";
-import {MenuItem} from "../../../services/menu.service";
+import {CartItem} from '../../../models/cart-item';
+import {MsngrService} from '../../../services/msngr.service';
+import {Product} from '../../../models/product';
+import {MenuItem} from '../../../services/menu.service';
 
 @Component({
   selector: 'app-cart-list',
@@ -13,63 +13,63 @@ export class CartListComponent implements OnInit {
 
   cartTotal = 0;
 
-  cartItems: CartItem[] = new Array()
+  cartItems: CartItem[] = new Array();
 
   constructor(private msg: MsngrService) { }
 
   ngOnInit(): void {
 
-    this.msg.getMsg().subscribe((product:any) => {
-      this.addProductToCart(product)
-    })
+    this.msg.getMsg().subscribe((product: any) => {
+      this.addProductToCart(product);
+    });
 
-    this.msg.getRemoveMsg().subscribe((product:any) => {
+    this.msg.getRemoveMsg().subscribe((product: any) => {
       this.removeProductFromCart(product);
-    })
+    });
   }
 
-  removeProductFromCart(cartItem: CartItem){
+  removeProductFromCart(cartItem: CartItem): void{
 
-    for(let i in this.cartItems) {
+    for (const i in this.cartItems) {
       if (this.cartItems[i].id === cartItem.id) {
-        if(this.cartItems[i].qty > 1){
-          this.cartItems[i].qty--
+        if (this.cartItems[i].qty > 1){
+          this.cartItems[i].qty--;
         } else {
           this.cartItems.forEach( (item, index) => {
-            if(item === cartItem) this.cartItems.splice(index,1);
-          })
+            if (item === cartItem) { this.cartItems.splice(index, 1); }
+          });
         }
       }
     }
 
   }
 
-  addProductToCart(product: MenuItem){
+  addProductToCart(product: MenuItem): void{
 
-    let productExists = false
+    let productExists = false;
 
-    console.log(product)
+    console.log(product);
 
-    for(let i in this.cartItems) {
+    for (const i in this.cartItems) {
       if (this.cartItems[i].id === product.pk) {
-        this.cartItems[i].qty++
-        productExists=true
+        this.cartItems[i].qty++;
+        productExists = true;
         break;
       }
     }
 
-    if(!productExists) {
+    if (!productExists) {
       this.cartItems.push({
         id: product.pk,
         name: product.name,
         qty: 1,
         price: product.price
-      })
+      });
     }
 
-    this.cartTotal = 0
+    this.cartTotal = 0;
     this.cartItems.forEach(item => {
-      this.cartTotal += (item.qty * item.price)
-    })
+      this.cartTotal += (item.qty * item.price);
+    });
   }
 }
